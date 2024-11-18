52e972e3dd1bdb1116d2f660ab0c3568b775a5de
Sam <30577766+Samasaur1@users.noreply.github.com>
2024-11-17 20:56:38 -0800

    update tests

--- Tests/TorrentFileKitTests/TorrentFileV2Tests.swift
+++ Tests/TorrentFileKitTests/TorrentFileV2Tests.swift
@@ -82,7 +82,7 @@ struct TorrentFileV2Tests {
         @Suite
         struct BitForBitIdenticalTests {
             @Test
-            func pureSingleFileV2DataToTorrentToData() async throws {
+            func pureSingleFileV2DataToInfoDictToData() async throws {
                 let u = try #require(Bundle.module.url(forResource: "v2.txt", withExtension: "infodict"))
                 let data = try Data(contentsOf: u)
 
@@ -99,7 +99,7 @@ struct TorrentFileV2Tests {
                 }
             }
             @Test
-            func pureMultipleFileV2DataToTorrentToData() async throws {
+            func pureMultipleFileV2DataToInfoDictToData() async throws {
                 let u = try #require(Bundle.module.url(forResource: "dirv2", withExtension: "infodict"))
                 let data = try Data(contentsOf: u)
 
@@ -116,7 +116,7 @@ struct TorrentFileV2Tests {
                 }
             }
             @Test
-            func pureSingleFileHybridDataToTorrentToData() async throws {
+            func pureSingleFileHybridDataToInfoDictToData() async throws {
                 let u = try #require(Bundle.module.url(forResource: "hybrid.txt", withExtension: "infodict"))
                 let data = try Data(contentsOf: u)
 
@@ -133,7 +133,7 @@ struct TorrentFileV2Tests {
                 }
             }
             @Test
-            func pureMultipleFileHybridDataToTorrentToData() async throws {
+            func pureMultipleFileHybridDataToInfoDictToData() async throws {
                 let u = try #require(Bundle.module.url(forResource: "dirhybrid", withExtension: "infodict"))
                 let data = try Data(contentsOf: u)
 
